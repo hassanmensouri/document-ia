c1ab6bfdb425c0914f9e4dfef9c499a730dd6871
Hassan Mensouri <hssnmensouri@gmail.com>
2026-05-02 12:40:01 +0200

    fix summary hf client params

--- app.py
+++ app.py
@@ -57,24 +57,16 @@ def clean_text(text):
     return text
 
 
-def summarize_with_hf(text, chunk_size=1200):
+def summarize_with_hf(text):
     if not HF_TOKEN:
         return "HF_TOKEN is missing"
-    text = clean_text(text)
 
-    summaries = []
+    result = client.summarization(
+        text[:5000],
+        model=HF_SUMMARY_MODEL
+    )
 
-    for i in range(0, min(len(text), 5000), chunk_size):
-        chunk = text[i:i + chunk_size]
-
-        result = client.summarization(
-            chunk,
-            model=HF_SUMMARY_MODEL
-        )
-
-        summaries.append(result.summary_text)
-
-    return " ".join(summaries)
+    return result.summary_text
 @app.get("/")
 def home():
     return {"message": "Document AI API is running"}
